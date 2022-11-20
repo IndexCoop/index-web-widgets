cc090b1e96fc32c56626b2e0b8417f1c0bd94d3d
teddy <chuangtheodore@gmail.com>
2022-11-20 11:38:32 +1100

    feat: Expose build for infra testing

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,8 @@ module.exports = {
   output: {
     path: __dirname + "/dist",
     publicPath: "/",
-    filename: "bundle.js",
+    // filename: "bundle.js",
+    filename: "index-web-widgets.js",
   },
   devServer: {
     static: {
