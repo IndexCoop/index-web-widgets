30f97ca736521af548e796763fc076a38a67b738
teddy <chuangtheodore@gmail.com>
2022-12-15 23:27:08 +1100

    feat: Update render to createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import App from './app';
 
-// Find all widget divs (in production there will likely only be a single widget per element)
-const WidgetDivs = document.querySelectorAll('.index_web_widget');
+// Find all widget targets (in production there will likely only be a single widget per element)
+const WidgetTargets = document.querySelectorAll('.index_web_widget');
 
-// Inject our React App into each
-WidgetDivs.forEach((Div) => {
-  ReactDOM.render(
+// Inject our React App into each widget
+WidgetTargets.forEach((widgetElement) => {
+  const root = createRoot(widgetElement!);
+  root.render(
     <ChakraProvider>
       <React.StrictMode>
-        <App domElement={Div} />
+        <App domElement={widgetElement} />
       </React.StrictMode>
-    </ChakraProvider>,
-    Div
+    </ChakraProvider>
   );
 });
