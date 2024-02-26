a0f532edaba4a8bb9ed3552d368ccdd4aaab302a
0xonramp <0xonramp@gmail.com>
2024-02-26 11:40:07 -0500

    feat: Index 2x products

--- src/widgets/token-line-charts/types.ts
+++ src/widgets/token-line-charts/types.ts
@@ -2,3 +2,9 @@ export type CdetiNavRow = {
   hour: string;
   NAV: number;
 };
+
+export type Index2xNavRow = {
+  hour: string;
+  btc2x_price: number;
+  eth2x_price: number;
+};
--- src/widgets/token-lines-charts-aprs/TokenAprsChartTooltip.tsx
+++ src/widgets/token-lines-charts-aprs/TokenAprsChartTooltip.tsx
@@ -5,20 +5,28 @@ import { colors } from '../../styles/colors';
 
 const TokenAprsChartTooltip = ({ active, payload, token }: any) => {
   if (active && payload && payload.length) {
-    const symbol = token?.symbol.toLowerCase()
-    const data = payload[0].payload
-    const { x: date, y } = data
+    const symbol = token?.symbol.toLowerCase();
+    const data = payload[0].payload;
+    const { x: date, y } = data;
 
     const items = [];
     if (symbol === 'dseth') {
-      items.push({ label: 'RETH', value: data.reth, color: colors.icGray3 })
-      items.push({ label: 'ETHx', value: data.eeth, color: colors.icBlue4 })
-      items.push({ label: 'WSTETH', value: data.wsteth, color: colors.icGray2 })
-      items.push({ label: 'swETH', value: data.sweth, color: colors.icBlue5 })
-      items.push({ label: 'osETH', value: data.oseth, color: colors.icBlue2 })
-      items.push({ label: 'sfrxETH', value: data.sfrxeth, color: colors.icBlue3 })
+      items.push({ label: 'RETH', value: data.reth, color: colors.icGray3 });
+      items.push({ label: 'ETHx', value: data.eeth, color: colors.icBlue4 });
+      items.push({
+        label: 'WSTETH',
+        value: data.wsteth,
+        color: colors.icGray2,
+      });
+      items.push({ label: 'swETH', value: data.sweth, color: colors.icBlue5 });
+      items.push({ label: 'osETH', value: data.oseth, color: colors.icBlue2 });
+      items.push({
+        label: 'sfrxETH',
+        value: data.sfrxeth,
+        color: colors.icBlue3,
+      });
     } else if (symbol === 'gtceth') {
-      items.push({ label: 'dsETH', value: data.dseth, color: colors.icGray4 })
+      items.push({ label: 'dsETH', value: data.dseth, color: colors.icGray4 });
     }
     return (
       <Flex
@@ -66,25 +74,30 @@ const TokenAprsChartTooltip = ({ active, payload, token }: any) => {
               {`${Number(y).toFixed(2)}%`}
             </Text>
           </Flex>
-          {items.map(item => (
-            <Flex direction='row' justifyContent='space-between' minWidth={10} key={item.label}>
-            <Text
-              color={item.color}
-              fontSize='12px'
-              fontWeight='500'
-              margin='0'
+          {items.map((item) => (
+            <Flex
+              direction='row'
+              justifyContent='space-between'
+              minWidth={10}
+              key={item.label}
             >
-              {item.label}
-            </Text>
-            <Text
-              color={item.color}
-              fontSize='12px'
-              fontWeight='500'
-              margin='0'
-            >
-              {`${Number(item.value).toFixed(2)}%`}
-            </Text>
-          </Flex>
+              <Text
+                color={item.color}
+                fontSize='12px'
+                fontWeight='500'
+                margin='0'
+              >
+                {item.label}
+              </Text>
+              <Text
+                color={item.color}
+                fontSize='12px'
+                fontWeight='500'
+                margin='0'
+              >
+                {`${Number(item.value).toFixed(2)}%`}
+              </Text>
+            </Flex>
           ))}
         </Flex>
       </Flex>
